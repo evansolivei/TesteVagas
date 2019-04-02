1cde8a702bbaeead52dc433b66ef7e9862c7056d
evansolivei <evansolivei@gmail.com>
2019-04-01 21:30:12 -0500

    Adicionando regras egress SecurityGroup

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,20 @@
 # Security Group Configs
 resource "aws_security_group" "vagas" {
   vpc_id = "${aws_vpc.default.default}"
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+  }
+
   name = "VagasTerraform"
 }
 
